f187cc2178983a5fbbb1ecf423fd73ff19464d58
Surya <47964646+nadarsurya@users.noreply.github.com>
2023-11-22 10:48:34 +0530

    Create pages.config.js

---
+++ pages.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // The production branch that Pages will deploy from
+  "build": {
+    "production": {
+      // The command to build your site
+      "command": "npm run build-js",
+      // The directory where your built site is located
+      "outputDir": "dist"
+    }
+  }
+}
